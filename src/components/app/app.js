c9107a57000a0ae10ae39ed2aad2872696914343
Georgy <marakhovskij@gmail.com>
2021-05-11 18:40:57 +0300

    toggle randomChar button.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,56 @@
-import React from 'react';
-import { Col, Row, Container } from 'reactstrap';
+import React, { Component } from 'react';
+import { Col, Row, Container, Button } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage/errorMessage';
 
 
-const App = () => {
-  return (
-    <>
-      <Container>
-        <Header />
-      </Container>
-      <Container>
-        <Row>
-          <Col lg={{ size: 5, offset: 0 }}>
-            <RandomChar />
-          </Col>
-        </Row>
-        <Row>
-          <Col md='6'>
-            <ItemList />
-          </Col>
-          <Col md='6'>
-            <CharDetails />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+export default class App extends Component {
+  state = {
+    showRandomChar: true,
+    error: false
+  }
+
+  toggleRandomChar = () => {
+    this.setState((state) => {
+      return {
+        showRandomChar: !state.showRandomChar
+      }
+    })
+  }
+
+  render() {
+    const {showRandomChar, error} = this.state;
+
+    if (error) {
+      return <ErrorMessage/>
+    }
+    const char = showRandomChar ? <RandomChar /> : null;
+
+    return (
+      <>
+        <Container>
+          <Header />
+        </Container>
+        <Container>
+          <Row>
+            <Col lg={{ size: 5, offset: 0 }}>
+              <Button color="primary" className="mb-2" onClick={this.toggleRandomChar}>Toggle Random character</Button>
+              {char}
+            </Col>
+          </Row>
+          <Row>
+            <Col md='6'>
+              <ItemList />
+            </Col>
+            <Col md='6'>
+              <CharDetails />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 };
-
-export default App;
