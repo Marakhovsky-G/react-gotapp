657778debfd1136b9678d12f5ee2af6053829505
Georgy <marakhovskij@gmail.com>
2021-05-12 17:39:11 +0300

    Рефакт полей персонажа, под универс. элемент.

--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
 import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import CharDetails, { Field } from '../charDetails/charDetails';
 import ErrorMessage from '../errorMessage/errorMessage';
 import GotService from '../../services/gotService';
 import RowBlock from '../rowBlock/rowBlock';
@@ -43,7 +43,12 @@ export default class CharacterPage extends Component {
     )
 
     const charDetails = (
-      <CharDetails charId={this.state.selectedChar} />
+      <CharDetails charId={this.state.selectedChar}>
+        <Field field="gender" label="Gender" />
+        <Field field="born" label="Born" />
+        <Field field="died" label="Died" />
+        <Field field="culture" label="Culture" />
+      </CharDetails>
     )
 
     return (
