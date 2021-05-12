e09046ef309ef92383bb2f96fe27eb28e8e0d60d
Georgy <marakhovskij@gmail.com>
2021-05-12 16:53:28 +0300

    рефакторинг для RowBlock

--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -5,6 +5,19 @@ import CharDetails from '../charDetails';
 import ErrorMessage from '../errorMessage/errorMessage';
 import GotService from '../../services/gotService';
 
+const RowBlock = ({ left, right }) => {
+  return (
+    <Row>
+      <Col md='6'>
+        {left}
+      </Col>
+      <Col md='6'>
+        {right}
+      </Col>
+    </Row>
+  )
+}
+
 export default class CharacterPage extends Component {
 
   gotService = new GotService();
@@ -30,20 +43,23 @@ export default class CharacterPage extends Component {
   render() {
 
     if (this.state.error) {
-      return <ErrorMessage/>
+      return <ErrorMessage />
     }
 
+    const itemList = (
+      <ItemList
+        onCharSelected={this.onCharSelected}
+        getData={this.gotService.getAllCharacters}
+        renderItem={({ name, gender }) => `${name} (${gender})`}
+      />
+    )
+
+    const charDetails = (
+      <CharDetails charId={this.state.selectedChar} />
+    )
+
     return (
-      <Row>
-        <Col md='6'>
-          <ItemList
-            onCharSelected={this.onCharSelected}
-            getData={this.gotService.getAllCharacters} />
-        </Col>
-        <Col md='6'>
-          <CharDetails charId={this.state.selectedChar} />
-        </Col>
-      </Row>
+      <RowBlock left={itemList} right={charDetails} />
     )
   }
 }
